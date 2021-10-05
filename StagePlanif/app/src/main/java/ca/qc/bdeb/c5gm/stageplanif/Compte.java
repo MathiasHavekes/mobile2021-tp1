94114a0039de7e5188945a491f68abf0be5a6366
Paquet-Rapold <1928107@bdeb.qc.ca>
2021-10-05 15:27:28 -0400

    MOB-2: creating recycler view backend

---
+++ StagePlanif/app/src/main/java/ca/qc/bdeb/c5gm/stageplanif/Compte.java
@@ -0,0 +1,49 @@
+package ca.qc.bdeb.c5gm.stageplanif;
+
+/**
+ * Le compte sont le type de donnees utilise pour le recycler view
+ */
+public class Compte {
+    /**
+     * Le nom du compte
+     */
+    private final String nom;
+    /**
+     * Le prénom du compte
+     */
+    private final String prenom;
+    /**
+     * La photo du compte
+     */
+    private Byte[] photo;
+    /**
+     * Le type de compte
+     */
+    private final Integer typeCompte;
+
+    /**
+     * Constructeur par défaut
+     * @param nom Le nom du compte
+     * @param prenom Le prénom du compte
+     * @param photo La photo du compte (a modifier)
+     * @param typeCompte Le type de compte
+     */
+    public Compte(String nom, String prenom, Byte[] photo, Integer typeCompte) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.photo = photo;
+        this.typeCompte = typeCompte;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public Integer getTypeCompte() {
+        return typeCompte;
+    }
+}
---
+++ StagePlanif/app/src/main/java/ca/qc/bdeb/c5gm/stageplanif/ListeCompteAdapter.java
@@ -0,0 +1,157 @@
+package ca.qc.bdeb.c5gm.stageplanif;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+/**
+ * Adapte la liste de compte pour le recycler view
+ */
+public class ListeCompteAdapter extends RecyclerView.Adapter<ListeCompteAdapter.ListeCompteHolder>{
+    /**
+     * La liste des comptes à adapter pour le recycler view
+     */
+    private ArrayList<Compte> listeCompte;
+    /**
+     * Un layout inflater
+     */
+    private final LayoutInflater inflater;
+    /**
+     * Les listener de cliques
+     */
+    private OnItemClickListener listener;
+
+    public ListeCompteAdapter(Context context, ArrayList<Compte> listeCompte) {
+        inflater = LayoutInflater.from(context);
+        this.listeCompte = listeCompte;
+    }
+
+    /**
+     * Interface qui demande de creer des methodes pour definir le comportements selon les listeners
+     */
+    public interface OnItemClickListener{
+        /**
+         * Comportement lors du clique pour mettre la priorité d'un contact
+         * @param position position dans le recyclerView de l'item
+         * @param favoriteView l'item qui a ete clique
+         */
+        void OnDrapeauClick(int position, ImageView favoriteView);
+
+        /**
+         * Comportement lors d'un clique sur le nom
+         * @param position position dans le recyclerView de l'item
+         */
+        void onNomClick(int position);
+
+        /**
+         * Comportement lors d'un clique sur l'image de l'élève
+         * @param position position dans le recyclerView de l'item
+         */
+        void OnImageEleveClick(int position);
+    }
+
+    /**
+     * Permet de setter les listeners
+     * @param listener
+     */
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ListeCompteHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = inflater.inflate(R.layout.student_list_item, parent, false);
+        return new ListeCompteHolder(itemView, this, listener);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListeCompteHolder holder, int position) {
+        Compte compte = listeCompte.get(position);
+        holder.nomEleveView.setText(compte.getPrenom() + " " + compte.getNom());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listeCompte.size();
+    }
+
+    /**
+     * Classe qui va afficher les views
+     */
+    public class ListeCompteHolder extends RecyclerView.ViewHolder{
+        /**
+         * TextView contenant le nom de l'élève
+         */
+        private TextView nomEleveView;
+        /**
+         * ImageView contenant le drapeau
+         */
+        private ImageView drapeauView;
+        /**
+         * ImageView contenant l'image de l'élève
+         */
+        private ImageView imageEleveView;
+        /**
+         * L'adapteur de la liste de compte
+         */
+        private final ListeCompteAdapter adapter;
+
+        public ListeCompteHolder(@NonNull View itemView, ListeCompteAdapter adapter, final OnItemClickListener listener) {
+            super(itemView);
+            nomEleveView = (TextView) itemView.findViewById(R.id.text_nom_eleve);
+            drapeauView = (ImageView) itemView.findViewById(R.id.image_drapeau);
+            imageEleveView = (ImageView) itemView.findViewById(R.id.image_eleve);
+            this.adapter = adapter;
+            setListeners(listener);
+        }
+
+        private void setListeners(final OnItemClickListener listener) {
+            drapeauView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.OnDrapeauClick(position, drapeauView);
+                        }
+                    }
+                }
+            });
+
+            nomEleveView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onNomClick(position);
+                        }
+                    }
+                }
+            });
+
+            imageEleveView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.OnImageEleveClick(position);
+                        }
+                    }
+                }
+            });
+        }
+
+    }
+
+}
