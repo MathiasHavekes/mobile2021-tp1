347240b11d8580692fdc7b3581a639fa72e39e0c
Xavier Paquet-Rapold <80051842+XavierPaquet-Rapold@users.noreply.github.com>
2021-10-05 17:13:03 -0400

    MOB-2: creating recycler view backend (#3)
    * MOB-2: creating recycler view backend
    
    * Enlever local.properties du git
    
    * Rearranger le code et ajouter getter et setter pour photo dans compte
    
    Co-authored-by: Paquet-Rapold <1928107@bdeb.qc.ca>

--- StagePlanif/app/src/main/java/ca/qc/bdeb/c5gm/stageplanif/MainActivity.java
+++ StagePlanif/app/src/main/java/ca/qc/bdeb/c5gm/stageplanif/MainActivity.java
@@ -7,8 +7,15 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private final ArrayList<Compte> listeCompte = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private ListeCompteAdapter compteAdapter;
     private Toolbar toolbar;
 
     @Override
@@ -16,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = findViewById((R.id.toolbar));
-
         setSupportActionBar(toolbar);
+        //Donnees de test, a enlever
+        listeCompte.add(new Compte("Paquet", "Xavier", null, 1));
+        listeCompte.add(new Compte("Havekes", "Mathias", null, 1));
+        listeCompte.add(new Compte("Hadjeres", "Amar", null, 1));
+        creationRecyclerView();
     }
 
     @Override
@@ -25,5 +36,15 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.activity_main_menu, menu);
         return true;
+    }
+
+    /**
+     * Cree le recycler view dans l'activitee
+     */
+    private void creationRecyclerView() {
+        recyclerView = findViewById(R.id.rv_eleves);
+        compteAdapter = new ListeCompteAdapter(this, listeCompte);
+        recyclerView.setAdapter(compteAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
