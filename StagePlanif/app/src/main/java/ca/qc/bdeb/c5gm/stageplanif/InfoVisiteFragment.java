3392f64ab6769c4b4d234a0f188649579b8d19ac
Xavier Paquet-Rapold <80051842+XavierPaquet-Rapold@users.noreply.github.com>
2021-11-18 14:56:28 -0500

    Debut fragment visite

--- StagePlanif/app/src/main/java/ca/qc/bdeb/c5gm/stageplanif/InfoVisiteFragment.java
+++ StagePlanif/app/src/main/java/ca/qc/bdeb/c5gm/stageplanif/InfoVisiteFragment.java
@@ -1,64 +1,113 @@
 package ca.qc.bdeb.c5gm.stageplanif;
 
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import java.time.LocalTime;
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link InfoVisiteFragment#newInstance} factory method to
- * create an instance of this fragment.
+ *
  */
 public class InfoVisiteFragment extends Fragment {
+    private EditText tempsDebutStage;
+    private EditText tempsFinStage;
+    private EditText tempsDebutDiner;
+    private EditText tempsFinDiner;
+    private RadioGroup radioGroup;
+    private TimePickerDialog dialogueChoixTemps;
+    private Context context;
+    private InfoStageViewModel viewModel;
+    private CheckBox checkBoxMercredi;
+    private CheckBox checkBoxJeudi;
+    private CheckBox checkBoxVendredi;
+    private View.OnClickListener choisirTemps = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            dialogueChoixTemps = new TimePickerDialog(context, (timePicker, heure, minute) -> mettreDate(heure, minute, view),
+                    0, 0, true);
+            dialogueChoixTemps.show();
+        }
+    };
+    private View.OnClickListener checkboxClique = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view.equals(checkBoxMercredi)) {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+            } else if (view.equals(checkBoxJeudi)) {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+            } else if (view.equals(checkBoxVendredi)) {
 
-    public InfoVisiteFragment() {
-        // Required empty public constructor
-    }
+            }
+        }
+    };
+    private RadioGroup.OnCheckedChangeListener radioGroupClique = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i) {
+            switch (i) {
+                case R.id.duree_30_minutes:
+                    viewModel.setTempsVisites(30);
+                    break;
+                case R.id.duree_45_minutes:
+                    viewModel.setTempsVisites(45);
+                    break;
+                case R.id.duree_60_minutes:
+                    viewModel.setTempsVisites(60);
+                    break;
+            }
+        }
+    };
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment InfoVisiteFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static InfoVisiteFragment newInstance(String param1, String param2) {
-        InfoVisiteFragment fragment = new InfoVisiteFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    private void mettreDate(int heure, int minutes, View view) {
+        LocalTime temps = LocalTime.of(heure, minutes);
+        if (view.equals(tempsDebutStage)) {
+            viewModel.setHeureDebutStage(temps);
+        } else if(view.equals(tempsFinStage)) {
+            viewModel.setHeureFinStage(temps);
+        } else if(view.equals(tempsDebutDiner)) {
+            viewModel.setHeureDebutDiner(temps);
+        } else if (view.equals(tempsFinDiner)) {
+            viewModel.setHeureFinDiner(temps);
+        }
+        EditText tempsText = (EditText) view;
+        String texteTemps = String.format("%02d:%02d", heure, minutes);
+        tempsText.setText(texteTemps);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        context = this.getContext();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_demande_info_visite, container, false);
+        View view = inflater.inflate(R.layout.fragment_demande_info_visite, container, false);
+        viewModel = new ViewModelProvider(requireActivity()).get(InfoStageViewModel.class);
+        tempsDebutStage = view.findViewById(R.id.text_stage_debut);
+        tempsDebutStage.setOnClickListener(choisirTemps);
+        tempsFinStage = view.findViewById(R.id.text_stage_fin);
+        tempsFinStage.setOnClickListener(choisirTemps);
+        tempsDebutDiner = view.findViewById(R.id.text_diner_debut);
+        tempsDebutDiner.setOnClickListener(choisirTemps);
+        tempsFinDiner = view.findViewById(R.id.text_diner_fin);
+        tempsFinDiner.setOnClickListener(choisirTemps);
+        checkBoxMercredi = view.findViewById(R.id.selection_mercredi);
+        checkBoxJeudi = view.findViewById(R.id.selection_jeudi);
+        checkBoxVendredi = view.findViewById(R.id.selection_vendredi);
+        radioGroup = view.findViewById(R.id.radio_group_temps);
+        radioGroup.setOnCheckedChangeListener(radioGroupClique);
+        return view;
     }
 }
